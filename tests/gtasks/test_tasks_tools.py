82f1fbf840830025c6d538af1f44e149db3c9bc3
Forrest Trepte <forresttrepte+github@hotmail.com>
2025-09-17 12:55:33 -0700

    list_tasks tests

--- tests/gtasks/test_tasks_tools.py
+++ tests/gtasks/test_tasks_tools.py
@@ -3,6 +3,10 @@ import webbrowser
 import pytest
 from fastmcp import Client
 
+# TODO: This assumes that a specific Google account is used for testing that has this particular task list.
+#   Ideally, there would be test setup that creates the task lists needed for testing.
+connector_test_task_list_ID = "d0R6V2pRalU5M0d2MkNFZQ"
+
 
 @pytest.mark.asyncio
 async def test_list_task_lists() -> None:
@@ -16,7 +20,8 @@ async def test_list_task_lists() -> None:
     async with client:
         result = await client.call_tool("list_task_lists", {})
         assert result is not None
-        assert "Connector test (ID: d0R6V2pRalU5M0d2MkNFZQ)" in result.data
+        assert f"Connector test (ID: {connector_test_task_list_ID})" in result.data
+        assert result.data.count("(ID: ") > 2
 
 
 @pytest.mark.asyncio
@@ -27,3 +32,61 @@ async def test_list_task_lists_max_results() -> None:
         assert result is not None
         # each task list in result has the string "(ID: <some id>)"
         assert result.data.count("(ID: ") == 2
+
+
+@pytest.mark.asyncio
+async def test_list_tasks() -> None:
+    client = Client("http://localhost:8111/mcp", auth="oauth")
+    async with client:
+        result = await client.call_tool(
+            "list_tasks",
+            {
+                "task_list_id": connector_test_task_list_ID,
+            },
+        )
+        assert result.data.count("(ID: ") > 2
+
+
+@pytest.mark.asyncio
+async def test_list_tasks_max_results() -> None:
+    client = Client("http://localhost:8111/mcp", auth="oauth")
+    async with client:
+        result = await client.call_tool(
+            "list_tasks",
+            {
+                "task_list_id": connector_test_task_list_ID,
+                "max_results": 2,
+            },
+        )
+        assert result.data.count("(ID: ") == 2
+
+
+@pytest.mark.asyncio
+async def test_list_tasks_show_completed() -> None:
+    client = Client("http://localhost:8111/mcp", auth="oauth")
+    async with client:
+        # Get without completed
+        result = await client.call_tool(
+            "list_tasks",
+            {
+                "task_list_id": connector_test_task_list_ID,
+                "show_completed": False,
+                "show_hidden": False,
+            },
+        )
+        assert "Status: completed" not in result.data
+        no_completed_count = result.data.count("(ID: ")
+        assert no_completed_count > 2
+
+        # Get with completed (and hidden, because the completed tasks are also hidden)
+        result = await client.call_tool(
+            "list_tasks",
+            {
+                "task_list_id": connector_test_task_list_ID,
+                "show_completed": True,
+                "show_hidden": True,
+            },
+        )
+        assert "Status: completed" in result.data
+        completed_count = result.data.count("(ID: ")
+        assert completed_count > no_completed_count
